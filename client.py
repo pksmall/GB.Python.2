543ec36c7421a330132f5858aff1ca7515308b31
Pavel Korzhenko <small@office-xxi.com>
2018-12-04 04:14:50 -0800

    lession 05 complite

--- client.py
+++ client.py
@@ -1,11 +1,9 @@
 # GB Python2 l03
 #
-import datetime
-import json
 import sys
-import time
-from socket import *
 import getopt
+from socket import *
+from client_log_config import *
 
 '''
 клиент отправляет запрос серверу;
@@ -16,22 +14,6 @@ addr — ip-адрес сервера; port — tcp-порт на сервере
 формирует ответ клиенту; отправляет ответ клиенту; имеет параметры командной строки: -p <port> — TCP-порт для работы
 (по умолчанию использует 7777); -a <addr> — IP-адрес для прослушивания (по умолчанию слушает все доступные адреса).
 '''
-
-CONF_FILE = '.config.json'
-confJson = None
-
-
-def load_config():
-    global confJson
-
-    with open("./" + CONF_FILE) as f_n:
-        f_n_content = f_n.read()
-        confJson = json.loads(f_n_content)
-
-
-def date(unixtime, format='%Y-%m-%d %H:%M:%S'):
-    d = datetime.datetime.fromtimestamp(unixtime)
-    return d.strftime(format)
 
 
 def usage():
@@ -63,11 +45,9 @@ def main(argv):
 
 
 def client(argv):
-    # config
-    load_config()
-
     # parse options
     main(argv)
+    logger.info("Client start.")
 
     try:
         s = socket(AF_INET, SOCK_STREAM)  # Создать сокет TCP
@@ -83,10 +63,15 @@ def client(argv):
             if 'action' in data_json and data_json['action'] == 'probe':
                 print('Действие сервера: ', data_json['action'], ', Время: ', date(data_json['time']),
                       ', Длина сообщения: ', len(data), ' байт')
+                logger.info('Действие сервера: {}, Время: {} Длина сообщения: {} байт'
+                             .format(data_json['action'], date(data_json['time']), len(data)))
             elif 'response' in data_json:
                 print('Сервер прислал: ', data_json['response'], ', Сообщение: ', data_json['alert'],
                       ', Длина сообщения: ', len(data), ' байт')
+                logger.info('Сервер прислал: {}, Сообщение: {}, Длина сообщения: {} байт'
+                             .format(data_json['response'], data_json['alert'], len(data)))
     except Exception as ex:
+        logger.critical('Message: {}'.format(ex))
         print('Connection Message:', ex)
         usage()
 
